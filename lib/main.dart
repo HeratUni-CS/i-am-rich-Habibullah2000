a42cd0a8b9746aced72443181716c58967bbf8e8
hu170habibullah <74875189+hu170habibullah@users.noreply.github.com>
2023-03-29 15:38:22 +0430

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.blueGrey,
-      appBar: AppBar(
-        backgroundColor: Colors.blueGrey[900],
-        title: const Center(
-          child: Text('I am rich'),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: const Center(
+            child: Text('I am rich'),
+          ),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/first.jpg'),
+          ),
         ),
       ),
     ),
-  ));
+  );
 }
